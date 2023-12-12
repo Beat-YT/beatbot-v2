3d5a5164a5bc6b8a8eaf33bc60d4cd83e26643ce
Beat YT <66485277+Beat-YT@users.noreply.github.com>
2023-12-11 21:48:40 -0500

    ok

--- build.js
+++ build.js
@@ -8,10 +8,14 @@ fse.copySync('files', 'build', { overwrite: true });
 fs.rmdirSync('build/js', {recursive:true});
 
 const jsFiles = fs.readdirSync('files/js');
-
+jsFiles.push(...fs.readdirSync('files/js/services').map(x => `services/${x}`));
+jsFiles.push(...fs.readdirSync('files/js/page').map(x => `page/${x}`));
 fs.mkdirSync('build/js')
+fs.mkdirSync('build/js/services')
+fs.mkdirSync('build/js/page')
 
 jsFiles.forEach(async x => {
+    if (!x.endsWith('.js')) return;
     var content = fs.readFileSync(`files/js/${x}`, 'utf-8');
 
     var obfuscationResult = JavaScriptObfuscator.obfuscate(
